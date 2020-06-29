04e8b46492bd2273425851ce839909a4dc2de23a
viswanath-dreams <viswanath-dreams@users.noreply.github.com>
2020-06-29 16:13:23 -0400

    Fixed_whitespace bug

--- Bullet_journal_v1.py
+++ Bullet_journal_v1.py
@@ -220,10 +220,10 @@ commands={
 def switch(case,notebook):
     if case == 'n':
         args={}
-        in_cmd=input("Please enter task name(optional tag,due_date). Format <name>,<due_date=yy-mm-dd>,<tag=new_tag>: ").replace(" ","").split(',')
+        in_cmd=input("Please enter task name(optional tag,due_date). Format <name>,<due_date=yy-mm-dd>,<tag=new_tag>: ").split(',')
         if len(in_cmd)>1:
             for i in range(1,len(in_cmd)):
-                key,val=in_cmd[i].split('=')
+                key,val=in_cmd[i].replace(" ","").split('=')
                 args.update({key:val})
         notebook.new_task(in_cmd[0],args)   
     elif case == 'd':
